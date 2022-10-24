21b1d8783fbf681641f27794187de13e17706688
Olivier Bourgeois <3271352+bourgeoisor@users.noreply.github.com>
2022-10-24 14:50:33 -0400

    Fix issues with dotnet quickstart container not building (#372)
    * Fix some issues with dotnet container
    
    * Fix remaining issues with dotnet quickstart
    
    * Remove stray comments

--- quickstart/dotnet/Startup.cs
+++ quickstart/dotnet/Startup.cs
@@ -17,6 +17,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace HelloGKE
 {
@@ -29,7 +30,7 @@ namespace HelloGKE
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
